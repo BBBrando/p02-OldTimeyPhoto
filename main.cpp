01dca2a61b8f730bff0175950132ba3874879f5e
BBBrando <bbrandolino@mail.csuchico.edu>
2017-10-02 17:45:09 -0700

    planning

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//Brendon Brandolino
+#include <iostream>
+#include <string>
+#include <vector>
+#include "bitmap.h"
+using namespace std;
+
+int main() {
+
+
+
+  return 0;
+}
+/*
+ask user to provide a .bmp
+if not tell the user to try again
+
+
+*/
