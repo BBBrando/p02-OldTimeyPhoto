575c52dc542f1b40f4f49a9af37cdf082441d14a
BBBrando <bbrandolino@mail.csuchico.edu>
2017-10-04 17:56:21 -0700

    version1

--- main.cpp
+++ main.cpp
@@ -6,7 +6,35 @@
 using namespace std;
 
 int main() {
+  Bitmap image;
+  vector<vector<Pixel> > bmp;
+  Pixel rgb;
+  string im;
 
+  cout<<"please enter the file name"<<endl;
+  cin>>im;
+  image.open(im);
+  bmp= image.toPixelMatrix();
+
+  cout<<bmp.size()<<endl;
+  cout<<bmp[0].size()<<endl;
+
+        for(int a = 0; a < bmp.size() ;a++)
+
+        {
+
+
+            for(int b = 0; b < bmp[a].size() ;b++)
+
+            {
+              cout<<bmp[a][b].red;
+              cout<<bmp[a][b].blue;
+              cout<<bmp[a][b].green;
+
+
+            }
+
+        }
 
 
   return 0;
