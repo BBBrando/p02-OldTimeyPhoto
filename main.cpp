3173bd57f2a430d47bfa18708854cd389c41f34e
BBBrando <bbrandolino@mail.csuchico.edu>
2017-10-16 20:43:43 -0700

    Final Version

--- main.cpp
+++ main.cpp
@@ -5,7 +5,15 @@
 #include "bitmap.h"
 using namespace std;
 
+const float lumRed = .2126;
+const float lumGreen = .7152;
+const float lumBlue = .0722;
+const int randomVariance = 50;
+const int maxNumVal = 255;
+
+
 int main() {
+
   int randomNumber;
   Bitmap image;
   vector<vector<Pixel> > bmp;
@@ -15,10 +23,16 @@ int main() {
   cout<<"please enter the file name"<<endl;
   cin>>im;
   image.open(im);
-  bmp= image.toPixelMatrix();
+    do {
+      cout<<"please enter a VALID file name"<<endl;
+      cin>>im;
+      image.open(im);
+    } while(image.isImage() == false);
 
+
+  bmp= image.toPixelMatrix();
   cout<<"it is "<<bmp.size()<<" pixels tall"<<endl;
-  cout<<"and"<<bmp[0].size()<<" pixels wide"<<endl;
+  cout<<"and "<<bmp[0].size()<<" pixels wide"<<endl;
 
   int r;
   int g;
@@ -34,16 +48,8 @@ int main() {
     cin>>film;
     srand ( time(NULL) );
 
-    cout<<"Do you want to add a border?"<<endl;
-    cout<<"(Y)es or (N)o"<<endl;
-    cin>>border;
-        if (border == 'y' || 'Y') {
-            cout<<"What size border do you want?"<<endl;
-            cin>>borderSize;
-        }
-
     cout<<"What algorithm would you like to use?"<<endl;
-    cout<<"(A)veraging, (S)ingle Channel, (L)uma"<<endl;
+    cout<<"(A)veraging, (S)ingle Channel, (L)uma, (T)errible"<<endl;
     cin>>style;
 
         for(int a = 0; a < bmp.size() ;a++)
@@ -68,20 +74,31 @@ int main() {
                     grey = g;
                   }
                   if (style == 'L') {
-                    grey = (r*.2126)+(g*.7152)+(u*.0722);
+                    grey = (r*lumRed)+(g*lumGreen)+(u*lumBlue);
+                  }
+                  if (style == 'T') {
+                    if ((r+g+u)/3 <(maxNumVal/5) ) {
+                      grey = maxNumVal/5;
+                    }
+                    if ((r+g+u)/3 <(2*maxNumVal/5) && (r+g+u)/3 >=(maxNumVal/5)) {
+                      grey = 2*maxNumVal/5;
+                    }
+                    if ((r+g+u)/3 <(3*maxNumVal/5) && (r+g+u)/3 >=(2*maxNumVal/5)) {
+                      grey = 3*maxNumVal/5;
+                    }
+                    if ((r+g+u)/3 <(4*maxNumVal/5) && (r+g+u)/3 >=(3*maxNumVal/5)) {
+                      grey = 4*maxNumVal/5;
+                    }
+                    if ((r+g+u)/3 <= maxNumVal && (r+g+u)/3 >=(4*maxNumVal/5)) {
+                      grey = maxNumVal;
+                    }
                   }
 
+                    if (film == 'Y' || film =='y') {
 
-
-                    if (film=='y') {  //fix issue with lowercase y
-                        film='Y';
-                      }
-
-                    if (film = 'Y' || 'y') {
-
-
-                      randomNumber = rand() % 20 ;
+                      randomNumber = rand() % randomVariance ;
                         grey = grey-randomNumber;
+                        grey = grey + (.5*randomVariance);
 
                       }
 
@@ -92,17 +109,20 @@ int main() {
                 grey = 0;
               }
 
-                rgb.red = grey;
-                rgb.blue = grey;
-                rgb.green = grey;
+              rgb.red = grey;
+              rgb.blue = grey;
+              rgb.green = grey;
 
 
-                bmp[a][b] = rgb;
+              bmp[a][b] = rgb;
+
+
+
             }
+
         }
 
 
-}
 
         image.fromPixelMatrix(bmp);
         image.save("oldtimey.bmp");
